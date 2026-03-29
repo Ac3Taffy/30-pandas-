eff71327c6bb5e2517e77724c8d54db1245f0796
永雏塔菲 <95995666+Ac3Taffy@users.noreply.github.com>
2026-03-30 02:02:31 +0800

    Create 1050. 合作过至少三次的演员或导演.py

---
+++ pandas/1050. 合作过至少三次的演员或导演.py
@@ -0,0 +1,45 @@
+# ActorDirector 表：
+
+# +-------------+---------+
+# | Column Name | Type    |
+# +-------------+---------+
+# | actor_id    | int     |
+# | director_id | int     |
+# | timestamp   | int     |
+# +-------------+---------+
+# timestamp 是这张表的主键(具有唯一值的列).
+ 
+
+# 编写解决方案找出合作过至少三次的演员和导演的 id 对 (actor_id, director_id)
+
+ 
+
+# 示例 1：
+
+# 输入：
+# ActorDirector 表：
+# +-------------+-------------+-------------+
+# | actor_id    | director_id | timestamp   |
+# +-------------+-------------+-------------+
+# | 1           | 1           | 0           |
+# | 1           | 1           | 1           |
+# | 1           | 1           | 2           |
+# | 1           | 2           | 3           |
+# | 1           | 2           | 4           |
+# | 2           | 1           | 5           |
+# | 2           | 1           | 6           |
+# +-------------+-------------+-------------+
+# 输出：
+# +-------------+-------------+
+# | actor_id    | director_id |
+# +-------------+-------------+
+# | 1           | 1           |
+# +-------------+-------------+
+# 解释：
+# 唯一的 id 对是 (1, 1)，他们恰好合作了 3 次。
+
+import pandas as pd
+
+def actors_and_directors(actor_director: pd.DataFrame) -> pd.DataFrame:
+  cnts = actor_director.groupby(['actor_id','director_id']).size().reset_index(name = 'count')
+  return cnts[cnts['count']>=3][['actor_id','director_id']]
